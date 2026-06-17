4b42aa63bf00dcd1a4bed2aeb7e917deaebdb020
Sidney Sissaoui <shideneyu@gmail.com>
2026-06-17 06:07:24 +0200

    rmux v0.6.0

--- Formula/rmux.rb
+++ Formula/rmux.rb
@@ -5,28 +5,32 @@
 class Rmux < Formula
   desc "Local terminal multiplexer with a tmux-style CLI and daemon runtime"
   homepage "https://rmux.io"
-  version "0.5.0"
-  url "https://github.com/Helvesec/rmux/releases/download/v0.5.0/rmux-0.5.0-macos-aarch64.tar.gz"
-  sha256 "283d1e3458daeb2168a2b9f760c200f3188eba38fb8c08977fd0c29c18410afc"
+  version "0.6.0"
+  url "https://github.com/Helvesec/rmux/releases/download/v0.6.0/rmux-0.6.0-macos-aarch64.tar.gz"
+  sha256 "731d9ade9f76ed75e3df5ba7887b3640d0f712446555d6a67d53923a649476cd"
   license any_of: ["MIT", "Apache-2.0"]
 
   depends_on :macos
 
   on_macos do
     on_arm do
-      url "https://github.com/Helvesec/rmux/releases/download/v0.5.0/rmux-0.5.0-macos-aarch64.tar.gz"
-      sha256 "283d1e3458daeb2168a2b9f760c200f3188eba38fb8c08977fd0c29c18410afc"
+      url "https://github.com/Helvesec/rmux/releases/download/v0.6.0/rmux-0.6.0-macos-aarch64.tar.gz"
+      sha256 "731d9ade9f76ed75e3df5ba7887b3640d0f712446555d6a67d53923a649476cd"
     end
 
     on_intel do
-      url "https://github.com/Helvesec/rmux/releases/download/v0.5.0/rmux-0.5.0-macos-x86_64.tar.gz"
-      sha256 "5c3228994849d12e13551eb016327bd37c70949b9b00e42bcd27dfdbd1a53400"
+      url "https://github.com/Helvesec/rmux/releases/download/v0.6.0/rmux-0.6.0-macos-x86_64.tar.gz"
+      sha256 "e5ea3a07836b33dd52685792489fb03c01db34b0d7a0db6cc2a9badb89e172c9"
     end
   end
 
   def install
     bin.install "bin/rmux"
+    bin.install "bin/rmux-daemon" if File.exist?("bin/rmux-daemon")
     man1.install "share/man/man1/rmux.1"
+    bash_completion.install "share/bash-completion/completions/rmux" if File.exist?("share/bash-completion/completions/rmux")
+    zsh_completion.install "share/zsh/site-functions/_rmux" if File.exist?("share/zsh/site-functions/_rmux")
+    fish_completion.install "share/fish/vendor_completions.d/rmux.fish" if File.exist?("share/fish/vendor_completions.d/rmux.fish")
     pkgshare.install "share/rmux/artifact-metadata.json" if File.exist?("share/rmux/artifact-metadata.json")
 
     license_files = Dir["LICENSE*"]
