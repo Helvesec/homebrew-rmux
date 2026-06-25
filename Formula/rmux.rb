16d3fdabf987ecb324871517ae2e2b9f4f1bf95d
Sidney Sissaoui <shideneyu@gmail.com>
2026-06-25 20:34:50 +0200

    rmux: update formula to v0.7.0
    Signed-off-by: Sidney Sissaoui <shideneyu@gmail.com>

--- Formula/rmux.rb
+++ Formula/rmux.rb
@@ -5,28 +5,29 @@
 class Rmux < Formula
   desc "Local terminal multiplexer with a tmux-style CLI and daemon runtime"
   homepage "https://rmux.io"
-  version "0.6.1"
-  url "https://github.com/Helvesec/rmux/releases/download/v0.6.1/rmux-0.6.1-macos-aarch64.tar.gz"
-  sha256 "098865b968a12d87fe5d25d877e69dfa77d4452b1776e8dd54c334e4152cc1e0"
+  version "0.7.0"
+  url "https://github.com/Helvesec/rmux/releases/download/v0.7.0/rmux-0.7.0-macos-aarch64.tar.gz"
+  sha256 "1db251126281afbc542c0a07bb598b4221a975ec2dc5f6c812f4c45babcbc84b"
   license any_of: ["MIT", "Apache-2.0"]
 
   depends_on :macos
 
   on_macos do
     on_arm do
-      url "https://github.com/Helvesec/rmux/releases/download/v0.6.1/rmux-0.6.1-macos-aarch64.tar.gz"
-      sha256 "098865b968a12d87fe5d25d877e69dfa77d4452b1776e8dd54c334e4152cc1e0"
+      url "https://github.com/Helvesec/rmux/releases/download/v0.7.0/rmux-0.7.0-macos-aarch64.tar.gz"
+      sha256 "1db251126281afbc542c0a07bb598b4221a975ec2dc5f6c812f4c45babcbc84b"
     end
 
     on_intel do
-      url "https://github.com/Helvesec/rmux/releases/download/v0.6.1/rmux-0.6.1-macos-x86_64.tar.gz"
-      sha256 "09dc8baceb900bbd161d7841d2436e08a643afe0734113402a8fa8d7f6466f33"
+      url "https://github.com/Helvesec/rmux/releases/download/v0.7.0/rmux-0.7.0-macos-x86_64.tar.gz"
+      sha256 "0fe3edd0b942ff707f237e46f4036237e40748dce554ce20d623530f91531498"
     end
   end
 
   def install
     bin.install "bin/rmux"
     bin.install "bin/rmux-daemon" if File.exist?("bin/rmux-daemon")
+    libexec.install "libexec/rmux" if File.exist?("libexec/rmux")
     man1.install "share/man/man1/rmux.1"
     bash_completion.install "share/bash-completion/completions/rmux" if File.exist?("share/bash-completion/completions/rmux")
     zsh_completion.install "share/zsh/site-functions/_rmux" if File.exist?("share/zsh/site-functions/_rmux")
