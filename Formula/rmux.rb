ceca4aa67360b1eb891c81670fb4d8d97a28f114
Sidney Sissaoui <shideneyu@gmail.com>
2026-06-27 03:08:16 +0200

    formula: update rmux to 0.7.1
    Signed-off-by: Sidney Sissaoui <shideneyu@gmail.com>

--- Formula/rmux.rb
+++ Formula/rmux.rb
@@ -5,22 +5,22 @@
 class Rmux < Formula
   desc "Local terminal multiplexer with a tmux-style CLI and daemon runtime"
   homepage "https://rmux.io"
-  version "0.7.0"
-  url "https://github.com/Helvesec/rmux/releases/download/v0.7.0/rmux-0.7.0-macos-aarch64.tar.gz"
-  sha256 "1db251126281afbc542c0a07bb598b4221a975ec2dc5f6c812f4c45babcbc84b"
+  version "0.7.1"
+  url "https://github.com/Helvesec/rmux/releases/download/v0.7.1/rmux-0.7.1-macos-aarch64.tar.gz"
+  sha256 "4fdcbee2b9e5c12f66f6a62626ca09abe79627a5881e03bfb15ac46ba6c1eee7"
   license any_of: ["MIT", "Apache-2.0"]
 
   depends_on :macos
 
   on_macos do
     on_arm do
-      url "https://github.com/Helvesec/rmux/releases/download/v0.7.0/rmux-0.7.0-macos-aarch64.tar.gz"
-      sha256 "1db251126281afbc542c0a07bb598b4221a975ec2dc5f6c812f4c45babcbc84b"
+      url "https://github.com/Helvesec/rmux/releases/download/v0.7.1/rmux-0.7.1-macos-aarch64.tar.gz"
+      sha256 "4fdcbee2b9e5c12f66f6a62626ca09abe79627a5881e03bfb15ac46ba6c1eee7"
     end
 
     on_intel do
-      url "https://github.com/Helvesec/rmux/releases/download/v0.7.0/rmux-0.7.0-macos-x86_64.tar.gz"
-      sha256 "0fe3edd0b942ff707f237e46f4036237e40748dce554ce20d623530f91531498"
+      url "https://github.com/Helvesec/rmux/releases/download/v0.7.1/rmux-0.7.1-macos-x86_64.tar.gz"
+      sha256 "92b917132cb63d52e255b055cd530370fed83ec793dca6343019a66c0c4e2d77"
     end
   end
 
